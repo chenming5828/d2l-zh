22d537c4580e621da7cd646fdcd470cb20010e4b
Sam Chen <chenming5828@126.com>
2026-09-21 19:56:45 +0800

    Modify dependencies and set Python version to 3.13
    Updated package requirements and minimum Python version.

--- setup.py
+++ setup.py
@@ -1,18 +1,43 @@
+# from setuptools import setup, find_packages
+# import d2l
+
+# requirements = [
+#     'jupyter==1.0.0',
+#     'numpy==1.21.5',
+#     'matplotlib==3.5.1',
+#     'requests==2.25.1',
+#     'pandas==1.2.4'
+# ]
+
+# setup(
+#     name='d2l',
+#     version=d2l.__version__,
+#     python_requires='>=3.5',
+#     author='D2L Developers',
+#     author_email='d2l.devs@gmail.com',
+#     url='https://d2l.ai',
+#     description='Dive into Deep Learning',
+#     license='MIT-0',
+#     packages=find_packages(),
+#     zip_safe=True,
+#     install_requires=requirements,
+# )
+
 from setuptools import setup, find_packages
 import d2l
 
 requirements = [
-    'jupyter==1.0.0',
-    'numpy==1.21.5',
-    'matplotlib==3.5.1',
-    'requests==2.25.1',
-    'pandas==1.2.4'
+    'jupyter>=1.1.1',
+    'numpy>=1.26.4',
+    'matplotlib>=3.9.0',
+    'requests>=2.32.3',
+    'pandas>=2.2.2'
 ]
 
 setup(
     name='d2l',
     version=d2l.__version__,
-    python_requires='>=3.5',
+    python_requires='>=3.10', # 修改最低版本，3.13满足
     author='D2L Developers',
     author_email='d2l.devs@gmail.com',
     url='https://d2l.ai',
@@ -22,3 +47,4 @@ setup(
     zip_safe=True,
     install_requires=requirements,
 )
+
